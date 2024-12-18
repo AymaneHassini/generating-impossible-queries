e9f1283cb5e3245bf7325ae19ee659892cadd23b
AymaneHassini <aymane.hassini@gmail.com>
2024-12-18 19:41:52 +0100

    Full Pipeline Example

--- models/classifier.py
+++ models/classifier.py
@@ -1,17 +1,41 @@
-from transformers import AutoModelForSequenceClassification, AutoTokenizer
-from transformers import Trainer
+from transformers import AutoModelForSequenceClassification, AutoTokenizer, BertForSequenceClassification, BertTokenizerFast, Trainer, TrainingArguments
+import torch
 
 checkpoint = "./checkpoint"
 
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-def load_classifier():
+training_args = TrainingArguments(
+     "tmp", 
+      disable_tqdm=True
+)
+
+def load_classifier_distilbert():
+
+    print(device)
     # Preparing the model
     classifier = AutoModelForSequenceClassification.from_pretrained(
         checkpoint, num_labels=3
-    )
+    ).to(device)
     tokenizer = AutoTokenizer.from_pretrained(checkpoint)
     trainer = Trainer(
         classifier,
+        args=training_args,
+    )
+
+    return trainer, tokenizer
+
+
+def load_classifier_bert():
+    # Preparing the model
+    classifier = BertForSequenceClassification.from_pretrained(
+      checkpoint,
+      num_labels=3,
+  )
+    tokenizer = BertTokenizerFast.from_pretrained(checkpoint, max_length=512)
+    trainer = Trainer(
+        classifier,
+        args=training_args,
     )
 
     return trainer, tokenizer
